677ff089180c513fc7406fab7899f135b522bddb
lyq248739402 <ari8409@foxmail.com>
2025-12-06 12:48:02 +0800

    布局调整

--- client/config/defaultSettings.ts
+++ client/config/defaultSettings.ts
@@ -15,9 +15,9 @@ const Settings: ProLayoutProps & {
   fixedHeader: false,
   fixSiderbar: true,
   colorWeak: false,
-  title: 'Ant Design Pro',
+  title: 'MiLiCard',  // 左上角标题
   pwa: true,
-  logo: 'https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg',
+  logo: '/mili.svg',  // 可替换为自己的 logo，放在 client/public/logo.svg
   iconfontUrl: '',
   token: {
     // 参见ts声明，demo 见文档，通过token 修改样式
